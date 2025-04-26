5df4df08f6d291baa39409d3763db928ef9c7273
saloni017 <salonisingh8228@gmail.com>
2025-04-26 19:09:42 +0530

    Update Kruskal.py

--- Kruskal.py
+++ Kruskal.py
@@ -1,29 +1,27 @@
 from functools import cmp_to_key
 
-def comparator(a,b):
-    return a[2] - b[2];
+def comparator(a, b):
+    return a[2] - b[2]
 
 def kruskals_mst(V, edges):
-
-    # Sort all edges
-    edges = sorted(edges,key=cmp_to_key(comparator))
+    # Sort all edges by weight
+    edges = sorted(edges, key=cmp_to_key(comparator))
     
-    # Traverse edges in sorted order
     dsu = DSU(V)
     cost = 0
     count = 0
+
     for x, y, w in edges:
-        
-        # Make sure that there is no cycle
         if dsu.find(x) != dsu.find(y):
             dsu.union(x, y)
             cost += w
             count += 1
             if count == V - 1:
                 break
+
     return cost
-    
-# Disjoint set data structure
+
+# Disjoint Set Union class
 class DSU:
     def __init__(self, n):
         self.parent = list(range(n))
@@ -46,9 +44,15 @@ class DSU:
                 self.parent[s2] = s1
                 self.rank[s1] += 1
 
-
 if __name__ == '__main__':
+    V = int(input("Enter number of vertices: "))
+    E = int(input("Enter number of edges: "))
     
-    # An edge contains, weight, source and destination
-    edges = [[0, 1, 10], [1, 3, 15], [2, 3, 4], [2, 0, 6], [0, 3, 5]]
-    print(kruskals_mst(4, edges))
+    edges = []
+    print("Enter edges in format: u v w")
+    for _ in range(E):
+        u, v, w = map(int, input().split())
+        edges.append([u, v, w])
+
+    cost = kruskals_mst(V, edges)
+    print(f"Minimum cost of MST is: {cost}")
